6516faac1b4d3a6a9f592d072b6ac5b0ba0f7a0f
narayan.ghimire <narayan.ghimire123@gmail.com>
2017-05-18 05:17:23 +0100

    my test

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -1,30 +1,22 @@
-/*
-Copyright IBM Corp 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
+}
+
+type User struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Balance  int    `json:"balance"`
 }
 
 func main() {
@@ -36,11 +28,188 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+
+	if len(args) != 6 {
+		return nil, errors.New("Incorrect number of arguments. Execting 6")
 	}
 
-	err := stub.PutState("hello_world", []byte(args[0]))
+	var usersArray []string
+
+	var userone User
+	userone.Name = args[0]
+	userone.Password = args[1]
+	balance, err := strconv.Atoi(args[2])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for asset holding at 3 place")
+	}
+
+	userone.Balance = balance
+
+	b, err := json.Marshal(userone)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for userone")
+	}
+
+	err = stub.PutState(args[0], b)
+	if err != nil {
+		return nil, err
+	}
+
+	userone.Name = args[3]
+	userone.Password = args[4]
+	balance, err = strconv.Atoi(args[5])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for asset holding at 3 place")
+	}
+
+	userone.Balance = balance
+
+	b, err = json.Marshal(userone)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for userone")
+	}
+
+	err = stub.PutState(args[3], b)
+	if err != nil {
+		return nil, err
+	}
+
+	usersArray = append(usersArray, args[0])
+	usersArray = append(usersArray, args[3])
+
+	b, err = json.Marshal(usersArray)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for usertwo")
+	}
+
+	err = stub.PutState("users", b)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (t *SimpleChaincode) Transaction(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	var X int // Transaction value
+	var err error
+
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+	}
+
+	// Get the state from the ledger
+	// TODO: will be nice to have a GetAllState call to ledger
+	Avalbytes, err := stub.GetState(args[0])
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+	var userA User
+	err = json.Unmarshal(Avalbytes, &userA)
+	if err != nil {
+		return nil, errors.New("Failed to marshal string to struct of userA")
+	}
+
+	Bvalbytes, err := stub.GetState(args[1])
+	if err != nil {
+		return nil, errors.New("Failed to get state")
+	}
+
+	var userB User
+	err = json.Unmarshal(Bvalbytes, &userB)
+	if err != nil {
+		return nil, errors.New("Failed to marshal string to struct of userB")
+	}
+
+	// Perform the execution
+	X, err = strconv.Atoi(args[2])
+	if err != nil {
+		return nil, errors.New("Third argument must be integer")
+	}
+
+	userA.Balance = userA.Balance - X
+	userB.Balance = userB.Balance + X
+	fmt.Printf("Aval = %d, Bval = %d\n", userA.Balance, userB.Balance)
+
+	b, err := json.Marshal(userA)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for usera")
+	}
+
+	// Write the state back to the ledger
+	err = stub.PutState(userA.Name, b)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err = json.Marshal(userB)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for userb")
+	}
+
+	err = stub.PutState(userB.Name, b)
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (t *SimpleChaincode) CreateUser(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3. name,password,balance to create user")
+	}
+
+	usersArray, err := stub.GetState("users")
+	if err != nil {
+		return nil, err
+	}
+
+	var users []string
+
+	err = json.Unmarshal(usersArray, &users)
+
+	if err != nil {
+		return nil, err
+	}
+
+	users = append(users, args[0])
+
+	b, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for usertwo")
+	}
+
+	err = stub.PutState("users", b)
+	if err != nil {
+		return nil, err
+	}
+
+	var userone User
+	userone.Name = args[0]
+	userone.Password = args[1]
+	balance, err := strconv.Atoi(args[2])
+	if err != nil {
+		return nil, errors.New("Expecting integer value for asset holding at 3 place")
+	}
+
+	userone.Balance = balance
+
+	b, err = json.Marshal(userone)
+	if err != nil {
+		fmt.Println(err)
+		return nil, errors.New("Errors while creating json string for userone")
+	}
+
+	err = stub.PutState(args[0], b)
 	if err != nil {
 		return nil, err
 	}
@@ -52,15 +221,13 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
-	// Handle different functions
-	if function == "init" {
-		return t.Init(stub, "init", args)
-	} else if function == "write" {
-		return t.write(stub, args)
+	if function == "transaction" {
+		return t.Transaction(stub, args)
+	} else if function == "create_user" {
+		return t.CreateUser(stub, args)
 	}
-	fmt.Println("invoke did not find func: " + function)
 
-	return nil, errors.New("Received unknown function invocation: " + function)
+	return nil, nil
 }
 
 // Query is our entry point for queries
@@ -70,6 +237,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	// Handle different functions
 	if function == "read" { //read a variable
 		return t.read(stub, args)
+	} else if function == "list_users" {
+		return t.listUsers(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)
 
@@ -113,3 +282,18 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 
 	return valAsbytes, nil
 }
+
+func (t *SimpleChaincode) listUsers(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var jsonResp string
+	var err error
+
+	valAsbytes, err := stub.GetState("users")
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for users}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security St
